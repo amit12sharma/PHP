57b6e7fbffacfc2e21b4bed9cbd21ba0ed56332a
amit12sharma <101698980+amit12sharma@users.noreply.github.com>
2022-06-04 09:26:06 +0530

    Add files via upload

---
+++ function_1.php
@@ -0,0 +1,16 @@
+<?php
+
+function greetings($festival = "Life")
+{
+    echo "Wish you a very Happy $festival";
+}
+echo "Amit<br>";
+echo "Hey Krish <br/>";
+greetings("Diwali");
+
+echo "<br/>";
+echo "Hey Rohan <br/>";
+
+greetings();
+
+?>
